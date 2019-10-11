79f3f423642a0db86700c2b869e5c32c463a9e73
Edson Ferreira <45494938+brasilsites@users.noreply.github.com>
2019-10-10 23:46:40 -0300

    Criado com Colaboratory

---
+++ teste.py
@@ -0,0 +1,52 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "teste.py",
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/brasilsites/codigos-em-python/blob/master/teste.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "1X_FeVMzs-Xq",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 34
+        },
+        "outputId": "8a36b2bb-90f8-4d63-ea33-e6af9c1f40a9"
+      },
+      "source": [
+        "print(\"Ola mundo\")"
+      ],
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "Ola mundo\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
