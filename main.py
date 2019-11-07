aa9be3aeea20c54216eca35609b06675cd8ed0a8
Edson Ferreira <45494938+brasilsites@users.noreply.github.com>
2019-11-06 21:09:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import Aula061120192 as p
+
+def main():
+    '''print(p.Pilha().getPilha())
+    posicao = input("Informe a cor para achar a posicao: ")
+    print(p.Pilha().buscaPosicao(posicao))
+    inserir = input("Informe a nova cor para inserir: ")
+    p.Pilha().inserirPosicao(inserir)
+    print(p.Pilha().getPilha())'''
+
+    '''seq=p.Sequencia()
+    insere = input("Informe a sequencia de caracteres: ")
+    seq.inserirDados(insere)
+    print(seq.imprimeDados())
+    print(seq.topo())'''
+
+    '''ins=p.Sequencia()
+    ins.insereNomeSobre(input("Informe o nome: "),input("Informe o sobrenome: "))
+    print(ins.imprimeDados())
+    ins.removeDados()
+    print(ins.imprimeDados())'''
+
+    listacid=p.Sequencia()
+    listacid.listaCidade()
+    print(listacid.imprimeDados())
+    print(listacid.topo())
+main()
